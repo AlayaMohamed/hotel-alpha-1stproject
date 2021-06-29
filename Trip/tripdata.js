a1d76d11844fa20650247fbb25a204dc5934f7cf
AlayaMohamed <alayamohamed@outlook.com>
2021-06-29 22:18:07 +0100

    started the trip page filling

--- Trip/tripdata.js
+++ Trip/tripdata.js
@@ -1,10 +1,15 @@
 var visits=[]
 $('#add').click(function (){
+    var result=false
 for(var i=0;i<visits.length;i++){
     if (visits[i]===($('#visits').val())){
-
+     result=true;
     }
 }
-
+if (result===false){
 visits.push($('#visits').val())
+}
+else
+alert("this destination is already selected")
 })
+
